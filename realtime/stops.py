966395a4c17e5af410051ec68f6cae9488ce5683
Jack Cook <hello@jackcook.nyc>
2015-07-26 15:38:00 -0400

    Parse stops data

---
+++ realtime/stops.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+def stops():
+    f = open('stops.txt', 'r')
+    data = f.read()
+
+    lines = data.split('\n')
+    stops = {}
+
+    first = False
+
+    for line in lines:
+        if first == False:
+            first = True
+            continue
+
+        stopdata = line.split(',')
+
+        if len(stopdata) > 1:
+            stops[stopdata[0]] = {
+                'stop_name': stopdata[2],
+                'stop_lat': stopdata[4],
+                'stop_lon': stopdata[5]
+            }
+
+    print json.dumps(stops, sort_keys=True, indent=4)
+
+stops()
