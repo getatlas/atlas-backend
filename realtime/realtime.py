75fa971cce7f81521f77210c80da376949fb299d
Jack Cook <hello@jackcook.nyc>
2015-07-26 15:27:40 -0400

    Sort realtime train data

--- realtime/realtime.py
+++ realtime/realtime.py
@@ -2,21 +2,30 @@
 
 from google.transit import gtfs_realtime_pb2 as gtfs
 import mta_pb2 as mta
+import sys
 import urllib
 
 mta_url = 'http://datamine.mta.info/files/k38dkwh992dk/gtfs'
 
 def update():
-    # f = open('gtfs', 'r')
-    # response = f.read()
-
     response = urllib.urlopen(mta_url)
 
     feed = gtfs.FeedMessage()
     feed.ParseFromString(response.read())
-    
+
+    entities = {}
+
     for entity in feed.entity:
-        if entity.HasField('trip_update'):
-            print entity
+        if entity.HasField('vehicle'):
+            route_id = entity.vehicle.trip.route_id
+            if route_id in entities.keys():
+                entities[route_id].append(entity)
+            else:
+                entities[route_id] = [entity]
+
+    for entity in entities[sys.argv[1]]:
+        print entity
+
+    print len(entities[sys.argv[1]])
 
 update()
