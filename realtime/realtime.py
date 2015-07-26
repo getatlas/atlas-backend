e758793dec2a3b316321641884dd1638d674a06a
Jack Cook <hello@jackcook.nyc>
2015-07-26 14:10:18 -0400

    Update stuff

--- realtime/realtime.py
+++ realtime/realtime.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from google.transit import gtfs_realtime_pb2 as gtfs
+import mta_pb2 as mta
 import urllib
 
 mta_url = 'http://datamine.mta.info/files/k38dkwh992dk/gtfs'
 
 def update():
-    feed = gtfs.FeedMessage()
+    # f = open('gtfs', 'r')
+    # response = f.read()
+
     response = urllib.urlopen(mta_url)
+
+    feed = gtfs.FeedMessage()
     feed.ParseFromString(response.read())
     
     for entity in feed.entity:
         if entity.HasField('trip_update'):
-            print entity.trip_update
+            print entity
+
+update()
